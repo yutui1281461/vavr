df0dc69cea44944c0201c6c00dfe6b561bcccce8
az950512 <qp3@illinois.edu>
2019-02-05 21:41:01 +0000

    BUILDID 285995332; TRY 0

---
+++ vavr/src/main/java/io/vavr/concurrent/FutureImpl.java
@@ -0,0 +1,366 @@
+/*  __    __  __  __    __  ___
+ * \  \  /  /    \  \  /  /  __/
+ *  \  \/  /  /\  \  \/  /  /
+ *   \____/__/  \__\____/__/
+ *
+ * Copyright 2014-2017 Vavr, http://vavr.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.vavr.concurrent;
+
+import io.vavr.CheckedConsumer;
+import io.vavr.CheckedFunction1;
+import io.vavr.collection.Queue;
+import io.vavr.control.Option;
+import io.vavr.control.Try;
+
+import java.util.Objects;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.LockSupport;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * <strong>INTERNAL API - This class is subject to change.</strong>
+ *
+ * @param <T> Result of the computation.
+ * @author Daniel Dietrich
+ */
+// TODO: where to use UncaughtExceptionHandler?
+final class FutureImpl<T> implements Future<T> {
+
+    /**
+     * Used to start new threads.
+     */
+    private final ExecutorService executorService;
+
+    /**
+     * Used to synchronize state changes.
+     */
+    private final Object lock = new Object();
+
+    /**
+     * Indicates if this Future is cancelled
+     */
+    @GuardedBy("lock")
+    private volatile boolean cancelled;
+
+    /**
+     * Once the Future is completed, the value is defined.
+     */
+    @GuardedBy("lock")
+    private volatile Option<Try<T>> value;
+
+    /**
+     * The queue of actions is filled when calling onComplete() before the Future is completed or cancelled.
+     * Otherwise actions = null.
+     */
+    @GuardedBy("lock")
+    private Queue<Consumer<Try<T>>> actions;
+
+    /**
+     * The queue of waiters is filled when calling await() before the Future is completed or cancelled.
+     * Otherwise waiters = null.
+     */
+    @GuardedBy("lock")
+    private Queue<Thread> waiters;
+
+    /**
+     * Once a computation is started via run(), job is defined and used to control the lifecycle of the computation.
+     * <p>
+     * The {@code java.util.concurrent.Future} is not intended to store the result of the computation, it is stored in
+     * {@code value} instead.
+     */
+    @GuardedBy("lock")
+    private java.util.concurrent.Future<?> job;
+
+    // single constructor
+    private FutureImpl(ExecutorService executorService, Option<Try<T>> value, Queue<Consumer<Try<T>>> actions, Queue<Thread> waiters, CheckedFunction1<FutureImpl<T>, java.util.concurrent.Future<?>> jobFactory) {
+        this.executorService = executorService;
+        synchronized (lock) {
+            this.cancelled = false;
+            this.value = value;
+            this.actions = actions;
+            this.waiters = waiters;
+            try {
+                this.job = jobFactory.apply(this);
+            } catch(Throwable x) {
+                tryComplete(Try.failure(x));
+            }
+        }
+    }
+
+    /**
+     * Creates a {@code FutureImpl} that is immediately completed with the given value. No task will be started.
+     *
+     * @param executorService An {@link ExecutorService} to run and control the computation and to perform the actions.
+     * @param value the result of this Future
+     */
+    @SuppressWarnings("unchecked")
+    static <T> FutureImpl<T> of(ExecutorService executorService, Try<? extends T> value) {
+        return new FutureImpl<>(executorService, Option.some(Try.narrow(value)), null, null, ignored -> null);
+    }
+
+    /**
+     * Creates a {@code FutureImpl} that is eventually completed.
+     * The given {@code computation} is <em>synchronously</em> executed, no thread is started.
+     *
+     * @param executorService  An {@link ExecutorService} to run and control the computation and to perform the actions.
+     * @param computation A non-blocking computation
+     * @param <T> value type of the Future
+     * @return a new {@code FutureImpl} instance
+     */
+    static <T> FutureImpl<T> sync(ExecutorService executorService, CheckedConsumer<Predicate<Try<? extends T>>> computation) {
+        return new FutureImpl<>(executorService, Option.none(), Queue.empty(), Queue.empty(), future -> {
+            computation.accept(future::tryComplete);
+            return null;
+        });
+    }
+
+    /**
+     * Creates a {@code FutureImpl} that is eventually completed.
+     * The given {@code computation} is <em>asynchronously</em> executed, a new thread is started.
+     *
+     * @param executorService  An {@link ExecutorService} to run and control the computation and to perform the actions.
+     * @param computation A (possibly blocking) computation
+     * @param <T> value type of the Future
+     * @return a new {@code FutureImpl} instance
+     */
+    static <T> FutureImpl<T> async(ExecutorService executorService, CheckedConsumer<Predicate<Try<? extends T>>> computation) {
+        // In a single-threaded context this Future may already have been completed during initialization.
+        return new FutureImpl<>(executorService, Option.none(), Queue.empty(), Queue.empty(), future -> executorService.submit(() -> {
+            try {
+                computation.accept(future::tryComplete);
+            } catch (Throwable x) {
+                future.tryComplete(Try.failure(x));
+            }
+        }));
+    }
+
+    @Override
+    public Future<T> await() {
+        if (!isCompleted()) {
+            _await(-1L, -1L, null);
+        }
+        return this;
+    }
+
+    @Override
+    public Future<T> await(long timeout, TimeUnit unit) {
+        final long now = System.nanoTime();
+        Objects.requireNonNull(unit, "unit is null");
+        if (timeout < 0) {
+            throw new IllegalArgumentException("negative timeout");
+        }
+        if (!isCompleted()) {
+            _await(now, timeout, unit);
+        }
+        return this;
+    }
+
+    /**
+     * Blocks the current thread.
+     * <p>
+     * If timeout = 0 then {@code LockSupport.park()} is called (start, timeout and unit are not used),
+     * otherwise {@code LockSupport.park(timeout, unit}} is called.
+     * <p>
+     * If a timeout > -1 is specified and the deadline is not met, this Future fails with a {@link TimeoutException}.
+     * <p>
+     * If this Thread was interrupted, this Future fails with a {@link InterruptedException}.
+     *
+     * @param start the start time in nanos, based on {@linkplain System#nanoTime()}
+     * @param timeout a timeout in the given {@code unit} of time
+     * @param unit a time unit
+     */
+    private void _await(long start, long timeout, TimeUnit unit) {
+        try {
+            ForkJoinPool.managedBlock(new ForkJoinPool.ManagedBlocker() {
+
+                final long duration = (unit == null) ? -1 : unit.toNanos(timeout);
+                final Thread thread = Thread.currentThread();
+
+                boolean threadEnqueued = false;
+
+                /**
+                 * Parks the Future's thread.
+                 * <p>
+                 * LockSupport.park() / parkNanos() may return when the Thread is permitted to be scheduled again.
+                 * If so, the Future's tryComplete() method wasn't called yet. In that case the block() method is
+                 * called again. The remaining timeout is recalculated accordingly.
+                 *
+                 * @return true, if this Future is completed, false otherwise
+                 * @throws InterruptedException not thrown by us, we complete the Future directly in that case
+                 */
+                @Override
+                public boolean block() throws InterruptedException {
+                    try {
+                        if (!threadEnqueued) {
+                            synchronized (lock) {
+                                waiters = waiters.enqueue(thread);
+                            }
+                            threadEnqueued = true;
+                        }
+                        if (timeout > -1) {
+                            final long delta = System.nanoTime() - start;
+                            final long remainder = duration - delta;
+                            LockSupport.parkNanos(remainder); // returns immediately if remainder <= 0
+                            if (System.nanoTime() - start > duration) {
+                                tryComplete(Try.failure(new TimeoutException("timeout after " + timeout + " " + unit)));
+                            }
+                        } else {
+                            LockSupport.park();
+                        }
+                        if (thread.isInterrupted()) {
+                            tryComplete(Try.failure(new InterruptedException()));
+                        }
+                    } catch(Throwable x) {
+                        tryComplete(Try.failure(x));
+                    }
+                    return isCompleted();
+                }
+                @Override
+                public boolean isReleasable() {
+                    return isCompleted();
+                }
+            });
+        } catch (Throwable x) {
+            tryComplete(Try.failure(x));
+        }
+    }
+
+    @Override
+    public Future<T> cancel(boolean mayInterruptIfRunning) {
+        if (!isCompleted()) {
+            synchronized (lock) {
+                Try.of(() -> job == null || job.cancel(mayInterruptIfRunning))
+                        .recover(ignored -> job != null && job.isCancelled())
+                        .onSuccess(cancelled -> {
+                            if (cancelled) {
+                                this.cancelled = tryComplete(Try.failure(new CancellationException()));
+                            }
+                        });
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public ExecutorService executorService() {
+        return executorService;
+    }
+
+    @Override
+    public Option<Try<T>> getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return value.isDefined();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Future<T> onComplete(Consumer<? super Try<T>> action) {
+        Objects.requireNonNull(action, "action is null");
+        if (isCompleted()) {
+            perform(action);
+        } else {
+            synchronized (lock) {
+                if (isCompleted()) {
+                    perform(action);
+                } else {
+                    actions = actions.enqueue((Consumer<Try<T>>) action);
+                }
+            }
+        }
+        return this;
+    }
+
+    // This class is MUTABLE and therefore CANNOT CHANGE DEFAULT equals() and hashCode() behavior.
+    // See http://stackoverflow.com/questions/4718009/mutable-objects-and-hashcode
+
+    @Override
+    public String toString() {
+        final Option<Try<T>> value = this.value;
+        final String s = (value == null || value.isEmpty()) ? "?" : value.get().toString();
+        return stringPrefix() + "(" + s + ")";
+    }
+
+    /**
+     * INTERNAL METHOD, SHOULD BE USED BY THE CONSTRUCTOR, ONLY.
+     * <p>
+     * Completes this Future with a value and performs all actions.
+     * <p>
+     * This method is idempotent. I.e. it does nothing, if this Future is already completed.
+     *
+     * @param value A Success containing a result or a Failure containing an Exception.
+     * @throws IllegalStateException if the Future is already completed or cancelled.
+     * @throws NullPointerException  if the given {@code value} is null.
+     */
+    private boolean tryComplete(Try<? extends T> value) {
+        Objects.requireNonNull(value, "value is null");
+        if (isCompleted()) {
+            return false;
+        } else {
+            final Queue<Consumer<Try<T>>> actions;
+            final Queue<Thread> waiters;
+            // it is essential to make the completed state public *before* performing the actions
+            synchronized (lock) {
+                if (isCompleted()) {
+                    actions = null;
+                    waiters = null;
+                } else {
+                    // the job isn't set to null, see isCancelled()
+                    actions = this.actions;
+                    waiters = this.waiters;
+                    this.value = Option.some(Try.narrow(value));
+                    this.actions = null;
+                    this.waiters = null;
+                    this.job = null;
+                }
+            }
+            if (waiters != null) {
+                waiters.forEach(this::unlock);
+            }
+            if (actions != null) {
+                actions.forEach(this::perform);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    private void perform(Consumer<? super Try<T>> action) {
+        try {
+            executorService.execute(() -> action.accept(value.get()));
+        } catch(Throwable x) {
+            // ignored // TODO: tell UncaughtExceptionHandler?
+        }
+    }
+
+    private void unlock(Thread waiter) {
+        try {
+            LockSupport.unpark(waiter);
+        } catch(Throwable x) {
+            // ignored // TODO: tell UncaughtExceptionHandler?
+        }
+    }
+}
---
+++ vavr/src/test/java/io/vavr/collection/HashMapTest.java
@@ -0,0 +1,160 @@
+/*  __    __  __  __    __  ___
+ * \  \  /  /    \  \  /  /  __/
+ *  \  \/  /  /\  \  \/  /  /
+ *   \____/__/  \__\____/__/
+ *
+ * Copyright 2014-2017 Vavr, http://vavr.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.vavr.collection;
+
+import io.vavr.Tuple2;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Spliterator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+public class HashMapTest extends AbstractMapTest {
+
+    @Override
+    protected String className() {
+        return "HashMap";
+    }
+
+    @Override
+    <T1, T2> java.util.Map<T1, T2> javaEmptyMap() {
+        return new java.util.HashMap<>();
+    }
+
+    @Override
+    protected <T1 extends Comparable<? super T1>, T2> HashMap<T1, T2> emptyMap() {
+        return HashMap.empty();
+    }
+
+    @Override
+    protected <K extends Comparable<? super K>, V, T extends V> Collector<T, ArrayList<T>, ? extends Map<K, V>> collectorWithMapper(Function<? super T, ? extends K> keyMapper) {
+        return HashMap.collector(keyMapper);
+    }
+
+    @Override
+    protected <K extends Comparable<? super K>, V, T> Collector<T, ArrayList<T>, ? extends Map<K, V>> collectorWithMappers(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends V> valueMapper) {
+        return HashMap.collector(keyMapper, valueMapper);
+    }
+
+    @Override
+    protected <T> Collector<Tuple2<Integer, T>, ArrayList<Tuple2<Integer, T>>, ? extends Map<Integer, T>> mapCollector() {
+        return HashMap.collector();
+    }
+
+    @SuppressWarnings("varargs")
+    @SafeVarargs
+    @Override
+    protected final <K extends Comparable<? super K>, V> HashMap<K, V> mapOfTuples(Tuple2<? extends K, ? extends V>... entries) {
+        return HashMap.ofEntries(entries);
+    }
+
+    @SuppressWarnings("varargs")
+    @SafeVarargs
+    @Override
+    protected final <K extends Comparable<? super K>, V> HashMap<K, V> mapOfEntries(java.util.Map.Entry<? extends K, ? extends V>... entries) {
+        return HashMap.ofEntries(entries);
+    }
+
+    @Override
+    protected <K extends Comparable<? super K>, V> HashMap<K, V> mapOf(K k1, V v1) {
+        return HashMap.of(k1, v1);
+    }
+
+    @Override
+    protected <K extends Comparable<? super K>, V> HashMap<K, V> mapOf(K k1, V v1, K k2, V v2) {
+        return HashMap.of(k1, v1, k2, v2);
+    }
+
+    @Override
+    protected <K extends Comparable<? super K>, V> HashMap<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3) {
+        return HashMap.of(k1, v1, k2, v2, k3, v3);
+    }
+
+    @Override
+    protected <T, K extends Comparable<? super K>, V> Map<K, V> mapOf(Stream<? extends T> stream, Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends V> valueMapper) {
+        return HashMap.ofAll(stream, keyMapper, valueMapper);
+    }
+
+    @Override
+    protected <T, K extends Comparable<? super K>, V> Map<K, V> mapOf(Stream<? extends T> stream, Function<? super T, Tuple2<? extends K, ? extends V>> f) {
+        return HashMap.ofAll(stream, f);
+    }
+
+    protected <K extends Comparable<? super K>, V> HashMap<K, V> mapOfNullKey(K k1, V v1, K k2, V v2) {
+        return mapOf(k1, v1, k2, v2);
+    }
+
+    @Override
+    protected <K extends Comparable<? super K>, V> HashMap<K, V> mapOfNullKey(K k1, V v1, K k2, V v2, K k3, V v3) {
+        return mapOf(k1, v1, k2, v2, k3, v3);
+    }
+
+    @Override
+    protected <K extends Comparable<? super K>, V> HashMap<K, V> mapTabulate(int n, Function<? super Integer, ? extends Tuple2<? extends K, ? extends V>> f) {
+        return HashMap.tabulate(n, f);
+    }
+
+    @Override
+    protected <K extends Comparable<? super K>, V> HashMap<K, V> mapFill(int n, Supplier<? extends Tuple2<? extends K, ? extends V>> s) {
+        return HashMap.fill(n, s);
+    }
+
+    // -- static narrow
+
+    @Test
+    public void shouldNarrowHashMap() {
+        final HashMap<Integer, Double> int2doubleMap = mapOf(1, 1.0d);
+        final HashMap<Number, Number> number2numberMap = HashMap.narrow(int2doubleMap);
+        final int actual = number2numberMap.put(new BigDecimal("2"), new BigDecimal("2.0")).values().sum().intValue();
+        assertThat(actual).isEqualTo(3);
+    }
+
+    @Test
+    public void shouldWrapMap() {
+        final java.util.Map<Integer, Integer> source = new java.util.HashMap<>();
+        source.put(1, 2);
+        source.put(3, 4);
+        assertThat(HashMap.ofAll(source)).isEqualTo(emptyIntInt().put(1, 2).put(3, 4));
+    }
+
+    // -- spliterator
+
+    @Test
+    public void shouldNotHaveSortedSpliterator() {
+        assertThat(of(1, 2, 3).spliterator().hasCharacteristics(Spliterator.SORTED)).isFalse();
+    }
+
+    @Test
+    public void shouldNotHaveOrderedSpliterator() {
+        assertThat(of(1, 2, 3).spliterator().hasCharacteristics(Spliterator.ORDERED)).isFalse();
+    }
+
+    // -- isSequential()
+
+    @Test
+    public void shouldReturnFalseWhenIsSequentialCalled() {
+        assertThat(of(1, 2, 3).isSequential()).isFalse();
+    }
+
+}
