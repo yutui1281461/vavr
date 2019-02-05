f2bff2693244f7986075ef0b60c07af8309e02d6
az950512 <qp3@illinois.edu>
2019-02-05 21:41:13 +0000

    BUILDID 353364157; TRY 0

--- vavr/src-gen/main/java/io/vavr/Tuple2.java
+++ vavr/src-gen/main/java/io/vavr/Tuple2.java
@@ -245,6 +245,58 @@ public final class Tuple2<T1, T2> implements Tuple, Comparable<Tuple2<T1, T2>>, 
         return List.of(_1, _2);
     }
 
+    public <T3> Tuple3<T1, T2, T3> concat(T3 t3) {
+        return Tuple.of(_1, _2, t3);
+    }
+
+    /**
+     * i=2
+     * j=1
+     */
+    public <T3> Tuple3<T1, T2, T3> concat(Tuple1<T3> tuple) {
+        return Tuple.of(_1, _2, tuple._1);
+    }
+
+    /**
+     * i=2
+     * j=2
+     */
+    public <T3, T4> Tuple4<T1, T2, T3, T4> concat(Tuple2<T3, T4> tuple) {
+        return Tuple.of(_1, _2, tuple._1, tuple._2);
+    }
+
+    /**
+     * i=2
+     * j=3
+     */
+    public <T3, T4, T5> Tuple5<T1, T2, T3, T4, T5> concat(Tuple3<T3, T4, T5> tuple) {
+        return Tuple.of(_1, _2, tuple._1, tuple._2, tuple._3);
+    }
+
+    /**
+     * i=2
+     * j=4
+     */
+    public <T3, T4, T5, T6> Tuple6<T1, T2, T3, T4, T5, T6> concat(Tuple4<T3, T4, T5, T6> tuple) {
+        return Tuple.of(_1, _2, tuple._1, tuple._2, tuple._3, tuple._4);
+    }
+
+    /**
+     * i=2
+     * j=5
+     */
+    public <T3, T4, T5, T6, T7> Tuple7<T1, T2, T3, T4, T5, T6, T7> concat(Tuple5<T3, T4, T5, T6, T7> tuple) {
+        return Tuple.of(_1, _2, tuple._1, tuple._2, tuple._3, tuple._4, tuple._5);
+    }
+
+    /**
+     * i=2
+     * j=6
+     */
+    public <T3, T4, T5, T6, T7, T8> Tuple8<T1, T2, T3, T4, T5, T6, T7, T8> concat(Tuple6<T3, T4, T5, T6, T7, T8> tuple) {
+        return Tuple.of(_1, _2, tuple._1, tuple._2, tuple._3, tuple._4, tuple._5, tuple._6);
+    }
+
     // -- Object
 
     @Override
